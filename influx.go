6429c4b5594299b5265a7cfb84606f67f54e309c
Francis Begyn <francis.begyn@gmail.com>
2019-01-27 20:24:11 +0100

    Working PoC, needs to be tested in a long running config for crashes

--- influx.go
+++ influx.go
@@ -21,11 +21,12 @@ func WriteLink(dbclient client.Client, db string, l netlink.Link) {
 		logrus.Errorf("Failed to create netlink link batch points: %v\n.", err)
 	}
 
-	tags := map[string]string{
-		"host": host,
+  tags := map[string]string{
+    "host": host,
     "name": l.Attrs().Name,
+    "type": l.Type(),
     "hwaddr": l.Attrs().HardwareAddr.String(),
-	}
+  }
 
   state := 0
   if l.Attrs().OperState.String() == "up" {
@@ -36,11 +37,11 @@ func WriteLink(dbclient client.Client, db string, l netlink.Link) {
     "operstate": state,
   }
 
-	influxPoint, err := client.NewPoint("link_stats", tags, fields, time.Now())
-	if err != nil {
-		logrus.Errorf("Failed to make a new link_stats point: %v.\n", err)
-	}
-	batch.AddPoint(influxPoint)
+  influxPoint, err := client.NewPoint("link_stats", tags, fields, time.Now())
+  if err != nil {
+    logrus.Errorf("Failed to make a new link_stats point: %v.\n", err)
+  }
+  batch.AddPoint(influxPoint)
 
 	err = dbclient.Write(batch)
 	if err != nil {
@@ -49,7 +50,7 @@ func WriteLink(dbclient client.Client, db string, l netlink.Link) {
 }
 
 // WriteQdisc writes the data from a qdisc to influx
-func WriteQdisc(dbclient client.Client, db string, q netlink.Qdisc) {
+func WriteQdisc(dbclient client.Client, db string, qdiscs *[]netlink.Qdisc) {
 	host, _ := os.Hostname()
 
 	batch, err := client.NewBatchPoints(client.BatchPointsConfig{
@@ -59,48 +60,37 @@ func WriteQdisc(dbclient client.Client, db string, q netlink.Qdisc) {
 		logrus.Errorf("Failed to create netlink qdisc batch points: %v\n.", err)
 	}
 
-	tags := map[string]string{
-		"host": host,
-    "linkindex": strconv.Itoa(q.Attrs().LinkIndex),
-    "handle": netlink.HandleStr(q.Attrs().Handle),
-    "parent": netlink.HandleStr(q.Attrs().Parent),
-	}
+  for _, q := range *qdiscs {
+    tags := map[string]string{
+      "host": host,
+      "linkindex": strconv.Itoa(q.Attrs().LinkIndex),
+      "type": q.Type(),
+      "handle": netlink.HandleStr(q.Attrs().Handle),
+      "parent": netlink.HandleStr(q.Attrs().Parent),
+    }
 
-  switch q.Type() {
-    case "hfsc":
-      qd := q.(*netlink.Hfsc)
-      fields := map[string]interface{}{
-        "default": qd.Defcls,
-      }
-      influxPoint, err := client.NewPoint("hfsc_qdisc", tags, fields, time.Now())
-      if err != nil {
-        logrus.Errorf("Failed to make a new hfsc qdisc point: %v.\n", err)
-      }
-      batch.AddPoint(influxPoint)
-    case "fq_codel":
-      qd := q.(*netlink.FqCodel)
-      fields := map[string]interface{}{
-        "target": qd.Target,
-        "limit": qd.Limit,
-        "interval": qd.Interval,
-        "ecn": qd.ECN,
-        "flows": qd.Flows,
-        "quantum": qd.Quantum,
-      }
-      influxPoint, err := client.NewPoint("fq_codel_qdisc", tags, fields, time.Now())
-      if err != nil {
-        logrus.Errorf("Failed to make a new fq_codel qdisc point: %v.\n", err)
-      }
-      batch.AddPoint(influxPoint)
-    default:
-      fields := map[string]interface{}{
-        "temp": 1,
-      }
-      influxPoint, err := client.NewPoint("qdisc", tags, fields, time.Now())
-      if err != nil {
-        logrus.Errorf("Failed to make a new qdisc point: %v.\n", err)
-      }
-      batch.AddPoint(influxPoint)
+    fields := map[string]interface{}{
+      "refcnt": q.Attrs().Refcnt,
+    }
+
+    switch q.Type() {
+      case "hfsc":
+        qd := q.(*netlink.Hfsc)
+        fields["default"] = qd.Defcls
+      case "fq_codel":
+        qd := q.(*netlink.FqCodel)
+        fields["target"] = qd.Target
+        fields["limit"] = qd.Limit
+        fields["interval"] = qd.Interval
+        fields["ecn"] = qd.ECN
+        fields["flows"] = qd.Flows
+        fields["quantum"] = qd.Quantum
+    }
+    influxPoint, err := client.NewPoint("qdisc_stats", tags, fields, time.Now())
+    if err != nil {
+      logrus.Errorf("Failed to make a new qdisc point: %v.\n", err)
+    }
+    batch.AddPoint(influxPoint)
   }
 
   err = dbclient.Write(batch)
@@ -110,7 +100,7 @@ func WriteQdisc(dbclient client.Client, db string, q netlink.Qdisc) {
 }
 
 // WriteClass writes the data to an influx db
-func WriteClass(dbclient client.Client, db string, c netlink.Class) {
+func WriteClass(dbclient client.Client, db string, classes *[]netlink.Class) {
 	host, _ := os.Hostname()
 
 	batch, err := client.NewBatchPoints(client.BatchPointsConfig{
@@ -120,67 +110,49 @@ func WriteClass(dbclient client.Client, db string, c netlink.Class) {
 		logrus.Errorf("Failed to create netlink class batch points: %v\n.", err)
 	}
 
-	tags := map[string]string{
-		"host": host,
-    "linkindex": strconv.Itoa(c.Attrs().LinkIndex),
-    "handle": netlink.HandleStr(c.Attrs().Handle),
-    "parent": netlink.HandleStr(c.Attrs().Parent),
-    "leaf": strconv.FormatUint(uint64(c.Attrs().Leaf), 10),
-	}
+  for _, c := range *classes{
+    tags := map[string]string{
+      "host": host,
+      "linkindex": strconv.Itoa(c.Attrs().LinkIndex),
+      "type": c.Type(),
+      "handle": netlink.HandleStr(c.Attrs().Handle),
+      "parent": netlink.HandleStr(c.Attrs().Parent),
+      "leaf": strconv.FormatUint(uint64(c.Attrs().Leaf), 10),
+    }
 
-  switch c.Type() {
-    case "hfsc":
-      cd := c.(*netlink.HfscClass)
-      Fburst, Fdelay, Frate := cd.Fsc.Attrs()
-      Uburst, Udelay, Urate := cd.Usc.Attrs()
-      Rburst, Rdelay, Rrate := cd.Rsc.Attrs()
-      Ffields := map[string]interface{}{
-        "burst": Fburst,
-        "delay": Fdelay,
-        "rate":  Frate,
-      }
-      influxPoint, err := client.NewPoint("hfsc_class_fsc", tags, Ffields, time.Now())
-      if err != nil {
-        logrus.Errorf("Failed to make a new hfsc class fsc point: %v.\n", err)
-      }
-      batch.AddPoint(influxPoint)
-      Rfields := map[string]interface{}{
-        "burst": Rburst,
-        "delay": Rdelay,
-        "rate":  Rrate,
-      }
-      influxPoint, err = client.NewPoint("hfsc_class_rsc", tags, Rfields, time.Now())
-      if err != nil {
-        logrus.Errorf("Failed to make a new hfsc class rsc point: %v.\n", err)
-      }
-      batch.AddPoint(influxPoint)
-      Ufields := map[string]interface{}{
-        "burst": Uburst,
-        "delay": Udelay,
-        "rate":  Urate,
-      }
-      influxPoint, err = client.NewPoint("hfsc_class_usc", tags, Ufields, time.Now())
-      if err != nil {
-        logrus.Errorf("Failed to make a new hfscl class usc point: %v.\n", err)
-      }
-      batch.AddPoint(influxPoint)
-    default:
-      fields := map[string]interface{}{
-        "bytes": c.Attrs().Statistics.Basic.Bytes,
-        "packets": c.Attrs().Statistics.Basic.Packets,
-        "backlog": c.Attrs().Statistics.Queue.Backlog,
-        "drops": c.Attrs().Statistics.Queue.Drops,
-        "overlimits": c.Attrs().Statistics.Queue.Overlimits,
-        "requeues": c.Attrs().Statistics.Queue.Requeues,
-        "qlen": c.Attrs().Statistics.Queue.Qlen,
-        "bps": c.Attrs().Statistics.RateEst.Bps,
-        "pps": c.Attrs().Statistics.RateEst.Pps,
-      }
-      influxPoint, err := client.NewPoint("class", tags, fields, time.Now())
-      if err != nil {
-        logrus.Errorf("Failed to make a new class point: %v.\n", err)
-      }
-      batch.AddPoint(influxPoint)
+    fields := map[string]interface{}{
+      "bytes": uint32(c.Attrs().Statistics.Basic.Bytes),
+      "packets": c.Attrs().Statistics.Basic.Packets,
+      "backlog": c.Attrs().Statistics.Queue.Backlog,
+      "drops": c.Attrs().Statistics.Queue.Drops,
+      "overlimits": c.Attrs().Statistics.Queue.Overlimits,
+      "requeues": c.Attrs().Statistics.Queue.Requeues,
+      "qlen": c.Attrs().Statistics.Queue.Qlen,
+      "bps": c.Attrs().Statistics.RateEst.Bps,
+      "pps": c.Attrs().Statistics.RateEst.Pps,
+    }
+
+    switch c.Type() {
+      case "hfsc":
+        cd := c.(*netlink.HfscClass)
+        Fburst, Fdelay, Frate := cd.Fsc.Attrs()
+        Uburst, Udelay, Urate := cd.Usc.Attrs()
+        Rburst, Rdelay, Rrate := cd.Rsc.Attrs()
+        fields["fsc_burst"] = Fburst
+        fields["fsc_delay"] = Fdelay
+        fields["fsc_rate"] = Frate
+        fields["rsc_burst"] = Rburst
+        fields["rsc_delay"] = Rdelay
+        fields["rsc_rate"] = Rrate
+        fields["usc_burst"] = Uburst
+        fields["usc_delay"] = Udelay
+        fields["usc_rate"] = Urate
+    }
+    influxPoint, err := client.NewPoint("class_stats", tags, fields, time.Now())
+    if err != nil {
+      logrus.Errorf("Failed to make a new class point: %v.\n", err)
+    }
+    batch.AddPoint(influxPoint)
   }
 
   err = dbclient.Write(batch)
