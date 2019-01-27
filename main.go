143281577e6aecda77324dd777406d66125089f9
Francis Begyn <francis.begyn@gmail.com>
2019-01-27 20:24:11 +0100

    Working PoC, needs to be tested in a long running config for crashes

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ var (
 	username   = kingpin.Flag("user", "Username for influxdb").Default("admin").Short('u').String()
 	password   = kingpin.Flag("password", "Password for influxdb").Default("admin").Short('k').String()
 	database      = kingpin.Flag("database", "Influxdb database to use").Default("netlink").Short('d').String()
-	port       = kingpin.Flag("port", "Influxdb data port").Default("8080").Short('p').Int16()
+	port       = kingpin.Flag("port", "Influxdb data port").Default("8086").Short('p').Int16()
 	host       = kingpin.Flag("host", "Influxdb server hostname").Default("localhost").Short('h').String()
 	interval       = kingpin.Flag("interval", "Interval to export the data").Default("15").Short('i').Int16()
 
@@ -42,23 +42,18 @@ func main() {
 	if err != nil {
     logrus.Fatalf("something went wrong with the influx connection: %v\n", err)
 	}
-  logrus.Infoln(dbclient)
+  logrus.Infoln("influx client started")
 
   ticker := time.NewTicker(time.Duration(*interval) * time.Second)
   for range ticker.C {
     link, qdiscs, classes := GetData(*interf)
     go WriteLink(dbclient, *database, link)
-    logrus.Infoln(qdiscs)
     go func(){
-      for _, qdisc := range qdiscs{
-        WriteQdisc(dbclient, *database, qdisc)
-      }
+      WriteQdisc(dbclient, *database, &qdiscs)
     }()
-    logrus.Infoln(classes)
     go func(){
-      for _, class := range classes{
-        WriteClass(dbclient, *database, class)
-      }
+      WriteClass(dbclient, *database, &classes)
     }()
+    logrus.Infoln("scrape completed")
   }
 }
