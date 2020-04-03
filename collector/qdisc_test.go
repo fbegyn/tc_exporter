5eac2cb4d1b2a6c8619d257bbc6286f29c53275f
Francis Begyn <francis@begyn.be>
2020-04-04 01:33:41 +0200

    redesigned qdisc exporter and implemented tests

---
+++ collector/qdisc_test.go
@@ -0,0 +1,53 @@
+package tc_collector
+
+import (
+	"net"
+	"os"
+	"testing"
+
+	"github.com/go-kit/kit/log"
+	"github.com/mdlayher/promtest"
+)
+
+func TestCollector(t *testing.T) {
+
+	tests := []struct {
+		name string
+	}{
+		{name: "dummy01"},
+		{name: "dummy1000"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rtnl, err := SetupDummyInterface(tt.name)
+			if err != nil {
+				t.Fatalf("could not setup dummy interface for testing: %v", err)
+			}
+			defer rtnl.Close()
+
+			interf, err := net.InterfaceByName(tt.name)
+			if err != nil {
+				t.Fatalf("could not get %s interface by name", tt.name)
+			}
+
+			var logger log.Logger
+			logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+			logger = log.With(logger, "test", "collector")
+
+			qc, err := NewQdiscCollector(interf, logger)
+			if err != nil {
+				t.Fatalf("failed to create qdisc collector for %s: %v", interf.Name, err)
+			}
+
+			body := promtest.Collect(t, qc)
+
+			if !promtest.Lint(t, body) {
+				t.Errorf("one or more promlint errors found")
+			}
+
+			rtnl.Link.Delete(1000)
+		})
+	}
+
+}
