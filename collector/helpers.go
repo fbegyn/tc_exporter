93482ea7a844b0cf3b0d5abd576a045a9efe7e0f
Francis Begyn <francis@begyn.be>
2020-04-20 22:13:13 +0200

    implement testing for the namespaces
    Implemented the testing for the namespace exporting. All that is left
    now is some refactoring and clean up. Thinking I'll merge this into
    master after these last 2 steps.
    
    The code seems solid and does what is expected from the features in this
    branch.
    
    Signed-off-by: Francis Begyn <francis@begyn.be>

--- collector/helpers.go
+++ collector/helpers.go
@@ -7,36 +7,11 @@ import (
 	"github.com/florianl/go-tc"
 	"github.com/jsimonetti/rtnetlink"
 	"github.com/mdlayher/netlink"
-	"golang.org/x/sys/unix"
 )
 
 // HandleStr is a simple helper function that cinstruct human readable handles
 func HandleStr(handle uint32) (uint32, uint32) {
 	return ((handle & 0xffff0000) >> 16), (handle & 0x0000ffff)
-}
-
-// SetupDummyInterface installs a temporary dummy interface
-func SetupDummyInterface(iface string, linkindex uint32) (*rtnetlink.Conn, error) {
-	con, err := rtnetlink.Dial(nil)
-	if err != nil {
-		return &rtnetlink.Conn{}, err
-	}
-
-	if err := con.Link.New(&rtnetlink.LinkMessage{
-		Family: unix.AF_UNSPEC,
-		Type:   unix.ARPHRD_NETROM,
-		Index:  linkindex,
-		Flags:  unix.IFF_UP,
-		Change: unix.IFF_UP,
-		Attributes: &rtnetlink.LinkAttributes{
-			Name: iface,
-			Info: &rtnetlink.LinkInfo{Kind: "dummy"},
-		},
-	}); err != nil {
-		return con, err
-	}
-
-	return con, err
 }
 
 // GetNetlinkConn gets a rtnetlink connection for the specified network namespace
