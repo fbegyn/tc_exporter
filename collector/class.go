860070e53201a9e61ccde9a2187fd036d5292a6e
Francis Begyn <francis@begyn.be>
2020-04-23 23:58:22 +0200

    expand the code documentation - class + test
    Started expanding and documentating some more code. Got through all of
    the `collector/class.go` file and through the `TestClassCollector` part
    of the test file for classes.
    
    Next up is the `TestServiceCurveCollector` part of the class test file
    and the other code files.
    
    Signed-off-by: Francis Begyn <francis@begyn.be>

--- collector/class.go
+++ collector/class.go
@@ -17,6 +17,7 @@ var (
 )
 
 // ClassCollector is the object that will collect Class data for the interface
+// It is a basic reperesentation of the Stats and Stats2 struct of iproute
 type ClassCollector struct {
 	logger     log.Logger
 	netns      map[string][]rtnetlink.LinkMessage
@@ -33,7 +34,7 @@ type ClassCollector struct {
 
 // NewClassCollector create a new ClassCollector given a network interface
 func NewClassCollector(netns map[string][]rtnetlink.LinkMessage, clog log.Logger) (prometheus.Collector, error) {
-	// Setup logger for qdisc collector
+	// Setup logger for the class collector
 	clog = log.With(clog, "collector", "class")
 	clog.Log("msg", "making class collector")
 
@@ -109,18 +110,26 @@ func (cc *ClassCollector) Describe(ch chan<- *prometheus.Desc) {
 
 // Collect fetches and updates the data the collector is exporting
 func (cc *ClassCollector) Collect(ch chan<- prometheus.Metric) {
+	// First we go and get the hostname of the system, so it can later be used in the labels
 	host, err := os.Hostname()
 	if err != nil {
 		cc.logger.Log("msg", "failed to get hostname", "err", err)
 	}
 
+	// start iterating over the defined namespaces and devices
 	for ns, devices := range cc.netns {
+		// interate over each device, TODO: maybe there is a more elegant way to do this then 2 for
+		// loops, I need a Go wizard to have a look at this.
 		for _, interf := range devices {
+			// Get all TC classes  for the specified device
 			classes, err := getClasses(uint32(interf.Index), ns)
 			if err != nil {
 				cc.logger.Log("msg", "failed to get classes", "interface", interf.Attributes.Name, "err", err)
 			}
 
+			// Range over each class and report the statisctics of the class to the channel for Prometheus
+			// metrics. Note that we print the handle with %x, so the hexadecimal notation. This way the
+			// reported labels match the output from `tc -s show class ...`
 			for _, cl := range classes {
 				handleMaj, handleMin := HandleStr(cl.Handle)
 				parentMaj, parentMin := HandleStr(cl.Parent)
@@ -240,7 +249,8 @@ func (cc *ClassCollector) Collect(ch chan<- prometheus.Metric) {
 
 }
 
-// ServiceCurveCollector is the object that will collect Service Curve data for the interface
+// ServiceCurveCollector is the object that will collect Service Curve data for the interface. It is
+// mainly used to determine the current limits imposed by the service curve
 type ServiceCurveCollector struct {
 	logger log.Logger
 	netns  map[string][]rtnetlink.LinkMessage
@@ -252,9 +262,11 @@ type ServiceCurveCollector struct {
 
 // NewServiceCurveCollector create a new ServiceCurveCollector given a network interface
 func NewServiceCurveCollector(netns map[string][]rtnetlink.LinkMessage, sclog log.Logger) (prometheus.Collector, error) {
+	// Set up the logger for the service curve collector
 	sclog = log.With(sclog, "collector", "hfsc")
 	sclog.Log("msg", "making SC collector")
 
+	// We need an object to persust the different types of curves in for each HFSC class
 	curves := make(map[string]*tc.ServiceCurve)
 
 	return &ServiceCurveCollector{
@@ -294,29 +306,41 @@ func (c *ServiceCurveCollector) Describe(ch chan<- *prometheus.Desc) {
 
 // Collect fetches and updates the data the collector is exporting
 func (c *ServiceCurveCollector) Collect(ch chan<- prometheus.Metric) {
+	// First we go and get the hostname of the system, so it can later be used in the labels
 	host, err := os.Hostname()
 	if err != nil {
 		c.logger.Log("msg", "failed to get hostname", "err", err)
 	}
 
+	// start iterating over the defined namespaces and devices
 	for ns, devices := range c.netns {
+		// interate over each device, TODO: maybe there is a more elegant way to do this then 2 for
+		// loops, I need a Go wizard to have a look at this.
 		for _, interf := range devices {
+			// Get all the classes for the interface
 			classes, err := getClasses(uint32(interf.Index), ns)
 			if err != nil {
 				c.logger.Log("msg", "failed to get classes", "interface", interf.Attributes.Name, "err", err)
 			}
 
+			// Iterate over each class
 			for _, cl := range classes {
 				handleMaj, handleMin := HandleStr(cl.Handle)
 				parentMaj, parentMin := HandleStr(cl.Parent)
 
-				if cl.Hfsc != nil {
-					c.curves["fsc"] = cl.Hfsc.Fsc
-					c.curves["rsc"] = cl.Hfsc.Rsc
-					c.curves["usc"] = cl.Hfsc.Usc
+				// If the class is a HFSC class, fetch the curve information. Otherwise continue to the next
+				// class since we are only intrested in HFSC here
+				if cl.Hfsc == nil {
+					continue
 				}
+				c.curves["fsc"] = cl.Hfsc.Fsc
+				c.curves["rsc"] = cl.Hfsc.Rsc
+				c.curves["usc"] = cl.Hfsc.Usc
 
+				// For each curve, report the settings of the service curve the channel
 				for typ, sc := range c.curves {
+					// If a certain type of curve is not defined, skip over it. It also functions as another
+					// safety check so the function does not error on nil curves
 					if sc == nil {
 						continue
 					}
@@ -363,12 +387,17 @@ func (c *ServiceCurveCollector) Collect(ch chan<- prometheus.Metric) {
 
 }
 
+// getClass fetches all the class of an interface in a certain network namespace (ns)
 func getClasses(devid uint32, ns string) ([]tc.Object, error) {
+	// Open a netlink TC socket for a specfied ns
 	sock, err := GetTcConn(ns)
 	if err != nil {
 		return nil, err
 	}
+	// when the socket is no longer used, we close it
 	defer sock.Close()
+
+	// Get all classes for a specified interface
 	classes, err := sock.Class().Get(&tc.Msg{
 		Family:  unix.AF_UNSPEC,
 		Ifindex: devid,
@@ -376,6 +405,11 @@ func getClasses(devid uint32, ns string) ([]tc.Object, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// This is acutal a little hack. Since we return all classes for the interface, and it is possible
+	// that HFSC class is used. Each flow through the HFSC class creates a fq_codel class with a random
+	// handle, the cardinality of these metrics becomes very high in Prometheus
+	// TODO: figure out a better solution for this, there has to be one
 	var cl []tc.Object
 	for _, class := range classes {
 		if class.Ifindex == devid && class.Kind != "fq_codel" {
