9a9d3ff58a1babb1bf468a726396a7fb1f9b7b3e
Francis Begyn <francis@begyn.be>
2020-04-23 23:58:22 +0200

    expand the code documentation - class + test
    Started expanding and documentating some more code. Got through all of
    the `collector/class.go` file and through the `TestClassCollector` part
    of the test file for classes.
    
    Next up is the `TestServiceCurveCollector` part of the class test file
    and the other code files.
    
    Signed-off-by: Francis Begyn <francis@begyn.be>

--- collector/class_test.go
+++ collector/class_test.go
@@ -13,8 +13,9 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+// TestClassCollector tests out the creation and polling of a ClassCollector
 func TestClassCollector(t *testing.T) {
-
+	// Define the test case
 	tests := []struct {
 		ns     string
 		name   string
@@ -30,31 +31,41 @@ func TestClassCollector(t *testing.T) {
 			// setup the netns for testing
 			if tt.ns != "default" {
 				shell(t, "ip", "netns", "add", tt.ns)
+				// later on, we delete the netns since it is no longer usefull
 				defer shell(t, "ip", "netns", "del", tt.ns)
 
+				// Check if the namespace was actually created
 				f, err := os.Open("/var/run/netns/" + tt.ns)
 				if err != nil {
 					t.Fatalf("failed to open namespace file: %v", err)
 				}
+				// Close the fd if we no longer need it
 				defer f.Close()
 			}
 
+			// create the dummy interface in the netns
 			rtnl, err := setupDummyInterface(t, tt.ns, tt.name, tt.linkid)
 			if err != nil {
 				t.Fatalf("could not setup %s interface for %s: %v", tt.name, tt.ns, err)
 			}
+			// close the returned rtnetlink connection if no longer needed
 			defer rtnl.Close()
 
+			// Fetch the test interface from the netns
 			interf, err := getLinkByName(tt.ns, tt.name)
 			if err != nil {
 				t.Logf("removing interface %s from %s\n", tt.name, tt.ns)
 				rtnl.Link.Delete(tt.linkid)
 				t.Fatalf("could not get %s interface by name", tt.name)
 			}
+
+			// Compose the test interface and netns "config"
 			test := make(map[string][]rtnetlink.LinkMessage)
+			// We fetcha ll devices in the default netns of the OS
 			con, _ := tcexporter.GetNetlinkConn("default")
 			links, _ := con.Link.List()
 			test["default"] = links
+			// We fetch all devices in the newly created netns
 			con, _ = tcexporter.GetNetlinkConn(tt.ns)
 			links, _ = con.Link.List()
 			test[tt.ns] = links
@@ -63,6 +74,7 @@ func TestClassCollector(t *testing.T) {
 			logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
 			logger = log.With(logger, "test", "collector")
 
+			// Create a ClassCollector with the test "config"
 			qc, err := tcexporter.NewClassCollector(test, logger)
 			if err != nil {
 				t.Logf("removing interface %s from %s\n", tt.name, tt.ns)
@@ -70,6 +82,7 @@ func TestClassCollector(t *testing.T) {
 				t.Fatalf("failed to create class collector for %s: %v", interf.Attributes.Name, err)
 			}
 
+			// Test out the functionality of the collector
 			body := promtest.Collect(t, qc)
 			if !promtest.Lint(t, body) {
 				t.Logf("removing interface %s from %s\n", tt.name, tt.ns)
@@ -83,6 +96,8 @@ func TestClassCollector(t *testing.T) {
 
 }
 
+// TestServiceCurveCollector tests out the creation and polling of a ServiceCurveCollector. It
+// rquires some more work because we need to set up a class that actually uses a Service curve
 func TestServiceCurveCollector(t *testing.T) {
 
 	tests := []struct {
